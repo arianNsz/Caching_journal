4830396f3dd76a3fa300d2e7902c78c3013b217e
ArianNsz <a.nasehzade@gmail.com>
2020-12-17 19:51:08 -0500

    start the A2C for transfer learning experiment

---
+++ A2C_transfer_learning.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 17 19:47:13 2020
+
+@author: arian92
+"""
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import layers
